44307feb9df25f48e26a04ccdfbd4e94ca83414f
TomaszOrpik <orpik.tomasz@gmail.com>
2020-10-11 13:45:28 +0200

    Lab1
    Laboratoria 1 - creating project + add company form

---
+++ NewBrandingStyleWeb/NewBrandingStyleWeb/Controllers/CompanyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyleWeb.Models;
+
+namespace NewBrandingStyleWeb.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfChartsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+
+            return View("CompanyAdded", viewModel);
+        }
+    }
+}
---
+++ NewBrandingStyleWeb/NewBrandingStyleWeb/Models/CompanyAddedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrandingStyleWeb.Models
+{
+    public class CompanyAddedViewModel
+    {
+        public int NumberOfCharsInName { get; set; }
+        public int NumberOfChartsInDescription { get; set; }
+        public bool IsHidden { get; set; }
+    }
+}
