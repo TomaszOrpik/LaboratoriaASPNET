348956cf69b60ff07e1656b16e13ab34bb3ca5d4
Tomasz Orpik <orpik.tomasz@gmail.com>
2020-10-25 13:47:15 +0100

    Lab 02 change of form + js confirmation

---
+++ NewBrandingStyleWeb/Controllers/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewBrandingStyleWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Controller : ControllerBase
+    {
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
