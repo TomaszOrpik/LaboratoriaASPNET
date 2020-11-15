e8b0818f3880b75c286a40fd12a0cc1840ca2a6f
Tomasz Orpik <orpik.tomasz@gmail.com>
2020-11-15 14:14:07 +0100

    Lab 3
    Added Entity framework and local sql express support

---
+++ NewBrandingStyleWeb/Controllers/ExchangesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using NewBrandingStyleWeb.Database;
+using NewBrandingStyleWeb.Entities;
+using NewBrandingStyleWeb.Models;
+
+namespace NewBrandingStyleWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExchangesController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly ExchangesDbContext _dbContext;
+        public ExchangesController(ExchangesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpPost]
+        public IActionResult Add(CompanyModel item)
+        {
+            var entity = new ItemEntity
+            {
+                Name = item.Name,
+                Description = item.Description,
+                IsVisible = item.IsVisible
+            };
+            _dbContext.Items.Add(entity);
+            _dbContext.SaveChanges();
+            return View(entity);
+        }
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
---
+++ NewBrandingStyleWeb/Database/ExchangesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewBrandingStyleWeb.Entities;
+
+namespace NewBrandingStyleWeb.Database
+{
+    public class ExchangesDbContext : DbContext
+    {
+        public ExchangesDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+        public DbSet<ItemEntity> Items { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // fluent configuration
+        }
+    }
+}
